47c017b378f2a952ebb69d2142bffa51f76369ce
Zac King <Zacharyking18@yahoo.com>
2016-03-02 13:59:30 -0600

    AnimControllers
    empty parents for anim

---
+++ Assets/Scripts/UnitScripts/CombatUnit.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CombatUnit : MonoBehaviour
+{
+    [SerializeField]
+    private int Health;
+    [SerializeField]
+    private int Attack;
+    [SerializeField]
+    private bool isDead = false;
+
+    // Use this for initialization
+    void Start ()
+    {
+        EventSystem.AddSubscriber(MessageType.COMBAT, "Hit",    new Callback<int>(onHit));
+        EventSystem.AddSubscriber(MessageType.COMBAT, "Dead",   new Callback(onDead));
+
+
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+	
+	}
+
+    void onHit(int damage)
+    {
+        Health -= damage;
+        if(Health < 0)
+        {
+            Health = 0;
+            EventSystem.Notify(MessageType.COMBAT, "Dead");
+        }
+    }
+
+    void onDead()
+    {
+
+    }
+
+    void onAttack()
+    {
+
+    }
+}
