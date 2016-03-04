adb5a829f15c9ad6d2b17c6f3b6ed52d386fc5d6
Zac KIng <Zacharyking18@yahoo.com>
2016-03-04 11:28:24 -0600

    Started battle flow

---
+++ Assets/Scripts/BattleFlow.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BattleFlow : MonoBehaviour
+{
+    List<Unit> battleUnits;
+    int currentTurnIndex = 0;
+
+    // Use this for initialization
+    void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(Input.GetKeyUp(KeyCode.Alpha1))
+        {
+
+        }
+	}
+
+    void PopUnits()
+    {
+
+    }
+    void OrderUnits()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+struct Effect
+{
+    UnitStat effectedStat;
+    int duration;
+    float modifer;
+}
+
+enum UnitStat
+{
+    HEALTH,
+    ATTACK,
+    DEFENCE,
+    SPEED
+}
+
+public class Unit : MonoBehaviour
+{
+    public float MaxHealth  = 10;
+    public float Health;
+    public float Attack     = 5;
+    public float Defence    = 2;
+    public float Speed      = 1;
+    public bool Alive       = true;
+
+    // Use this for initialization
+    void Start ()
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void onHit()
+    {
+
+    }
+
+    void onAttack()
+    {
+
+    }
+
+    void onDeath()
+    {
+
+    }
+
+
+
+
+}
